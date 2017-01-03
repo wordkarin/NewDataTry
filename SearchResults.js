c70ede75fcd7431f519dcc8f10ca335d7589a3eb
Karin Kubischta <karin.kubischta@gmail.com>
2017-01-03 15:55:57 -0800

    added pretty styles to search results, pulling in specific data and images

--- SearchResults.js
+++ SearchResults.js
@@ -21,14 +21,30 @@ class SearchResults extends Component {
   }
 
   renderRow(rowData, sectionID, rowID) {
+    var price = rowData.price_formatted.split(' ')[0];
     return (
-      <TouchableHighlight
+      <TouchableHighlight onPress={() => this.rowPressed(rowData.lister_url)}
         underlayColor="#dddddd">
         <View>
-          <Text>{rowData.title}</Text>
+          <View style={styles.rowContainer}>
+            <Image style={styles.thumb} source={{uri: rowData.img_url}} />
+            <View style={styles.textContainer}>
+              <Text style={styles.price}>
+                {price}
+              </Text>
+              <Text style={styles.title}
+                numberOfLines={1}>
+                {rowData.title}
+              </Text>
+            </View>
+          </View>
         </View>
       </TouchableHighlight>
     );
+  }
+
+  rowPressed(listerURL){
+    var property = this.props.listings.filter(prop => prop.lister_url === listerURL)[0];
   }
 
   render(){
@@ -40,4 +56,31 @@ class SearchResults extends Component {
   }
 }
 
+var styles = StyleSheet.create({
+  thumb: {
+    width: 80,
+    height: 80,
+    marginRight: 10
+  },
+  textContainer: {
+    flex: 1
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#dddddd'
+  },
+  price: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#48BBEC'
+  },
+  title: {
+    fontSize: 20,
+    color: '#656565'
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    padding: 10
+  }
+});
 module.exports = SearchResults;
