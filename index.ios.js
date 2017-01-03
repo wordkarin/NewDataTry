51ebea4ea840dc334116a3d1f975be892df84ec3
Karin Kubischta <karin.kubischta@gmail.com>
2017-01-03 12:15:04 -0800

    practicing using models to make schemas

--- index.ios.js
+++ index.ios.js
@@ -14,17 +14,56 @@ import {
 
 const Realm = require('realm');
 
+
+// Realm data models are defined by the schema information passed into a Realm during initialization. The schema for an object consists of the object’s name and a set of properties each of which has a name and type as well as the objectType for object and list properties. You can also designate each property to be optional or to have a default value.
+
+const CarSchema = {
+  name: 'Car',
+  properties: {
+    make:  'string',
+    model: 'string',
+    miles: {type: 'int', default: 0},
+  }
+};
+
+const PersonSchema = {
+  name: 'Person',
+  properties: {
+    name:     'string',
+    birthday: 'date',
+    cars:     {type: 'list', objectType: 'Car'},
+    picture:  {type: 'data', optional: true}, // optional property
+  }
+};
+
+// Initialize a Realm with Car and Person models
+let carRealm = new Realm({schema: [CarSchema, PersonSchema]});
+
+carRealm.write(() => {
+  let car = carRealm.create('Car', {
+    make: 'Honda',
+    model: 'Civic',
+    miles: 750,
+  });
+
+  // you can access and set all properties defined in your model
+  console.log('>>>>> Car type is ' + car.make + ' ' + car.model);
+  console.log('>>>>> Car miles is: ' + car.miles);
+  car.miles = 1500;
+  console.log('>>>>> Car miles is: ' + car.miles);
+});
+
 class NewDataTry extends Component {
   render() {
-    let realm = new Realm({
-     schema: [{name: 'Dog', properties: {name: 'string'}}]
-    });
-
-    realm.write(() => {
-      realm.create('Dog', {name: 'Rex'});
-    });
-
-    console.log(realm.objects('Dog')); 
+    // let realm = new Realm({
+    //  schema: [{name: 'Dog', properties: {name: 'string'}}]
+    // });
+    //
+    // realm.write(() => {
+    //   realm.create('Dog', {name: 'Rex'});
+    // });
+    //
+    // console.log(realm.objects('Dog'));
 
     return (
       <View style={{flex:1}}>
@@ -42,7 +81,7 @@ class NewDataTry extends Component {
         </View>
         <View style={styles.container}>
           <Text style={styles.welcome}>
-            Count of Dogs in Realm: {realm.objects('Dog').length}
+            Count of Dogs in Realm: {/*realm.objects('Dog').length*/}
           </Text>
         </View>
       </View>
