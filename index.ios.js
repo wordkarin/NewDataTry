16db8bf1b11e5578449d730f51fddb833c4af725
Karin Kubischta <karin.kubischta@gmail.com>
2017-01-03 11:07:39 -0800

    realm is working, apparently.

---
+++ index.ios.js
@@ -0,0 +1,70 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+const Realm = require('realm');
+
+class NewDataTry extends Component {
+  render() {
+    let realm = new Realm({
+     schema: [{name: 'Dog', properties: {name: 'string'}}]
+    });
+
+    realm.write(() => {
+      realm.create('Dog', {name: 'Rex'});
+    });
+
+    return (
+      <View style={{flex:1}}>
+        <View style={styles.container}>
+          <Text style={styles.welcome}>
+            Welcome to React Native!
+          </Text>
+          <Text style={styles.instructions}>
+            To get started, edit index.ios.js
+          </Text>
+          <Text style={styles.instructions}>
+            Press Cmd+R to reload,{'\n'}
+            Cmd+D or shake for dev menu
+          </Text>
+        </View>
+        <View style={styles.container}>
+          <Text style={styles.welcome}>
+            Count of Dogs in Realm: {realm.objects('Dog').length}
+          </Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('NewDataTry', () => NewDataTry);
