0536aac6c6c9106293e5de140d78b7d40018bbf8
Karin Kubischta <karin.kubischta@gmail.com>
2017-01-04 14:03:52 -0800

    fixing some ES6 syntax in index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -5,54 +5,53 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
-  Text,
-  View,
   NavigatorIOS,
-  TouchableHighlight,
-  ActivityIndicator
 } from 'react-native';
 
-var SearchPage = require('./SearchPage');
+const SearchPage = require('./SearchPage');
 const Realm = require('realm');
 
 
-// Realm data models are defined by the schema information passed into a Realm during initialization. The schema for an object consists of the object’s name and a set of properties each of which has a name and type as well as the objectType for object and list properties. You can also designate each property to be optional or to have a default value.
+//Realm data models are defined by the schema information passed into a Realm during initialization. The schema for an object consists of the object’s name and a set of properties each of which has a name and type as well as the objectType for object and list properties. You can also designate each property to be optional or to have a default value.
 
-const CarSchema = {
-  name: 'Car',
+const HangSchema = {
+  name: 'Hang',
   properties: {
-    make:  'string',
-    model: 'string',
-    miles: {type: 'int', default: 0},
+    date: 'string',
+    duration: { type: 'int', default: 0 },
   }
 };
 
-const PersonSchema = {
-  name: 'Person',
+const HoldSchema = {
+  name: 'Hold',
   properties: {
-    name:     'string',
-    birthday: 'date',
-    cars:     {type: 'list', objectType: 'Car'},
-    picture:  {type: 'data', optional: true}, // optional property
+    name: 'string',
+    hangs: { type: 'list', objectType: 'Hang' },
+    picture: { type: 'data', optional: true }, // optional property
   }
 };
 
 // Initialize a Realm with Car and Person models
-let carRealm = new Realm({schema: [CarSchema, PersonSchema]});
+const hangRealm = new Realm({ schema: [HangSchema, HoldSchema] });
 
-carRealm.write(() => {
-  let car = carRealm.create('Car', {
-    make: 'Honda',
-    model: 'Civic',
-    miles: 750,
+
+hangRealm.write(() => {
+  const smallCrimp = hangRealm.create('Hold', {
+    name: 'Small Crimp'
   });
 
-  // you can access and set all properties defined in your model
-  console.log('>>>>> Car type is ' + car.make + ' ' + car.model);
-  console.log('>>>>> Car miles is: ' + car.miles);
-  car.miles = 1500;
-  console.log('>>>>> Car miles is: ' + car.miles);
+  const hangList = smallCrimp.hangs;
+
+  hangList.push({ date: '1/2/2016', duration: 10 });
+  hangList.push({ date: '1/3/2016', duration: 11 });
+  hangList.push({ date: '1/4/2016', duration: 11 });
 });
+
+  // you can access and set all properties defined in your model
+  const smallCrimp = hangRealm.objects('Hold');
+
+  console.log(smallCrimp);
+
 
 class PropertyFinderApp extends Component {
   render() {
@@ -62,12 +61,13 @@ class PropertyFinderApp extends Component {
         initialRoute={{
           title: 'Property Finder',
           component: SearchPage,
-        }}/>
+        }}
+      />
     );
   }
 }
 
-var styles = StyleSheet.create({
+const styles = StyleSheet.create({
   text: {
     color: 'black',
     backgroundColor: 'white',
